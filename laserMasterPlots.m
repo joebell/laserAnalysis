57eba29d4ead790e64bd8c6a9cc75c040f79bf41
Joe Bell <joe.bell@gmail.com>
2013-11-26 21:55:14 -0500

    Set figures to visible-off for orchestra

--- laserMasterPlots.m
+++ laserMasterPlots.m
@@ -1,14 +1,14 @@
 function laserMasterPlots(plotTitle, expList, useEpochs,useLanes, fileName)
 
-    h1 = figure();
+    h1 = figure('visible','off');
     laserMasterPlot1(plotTitle, expList, useEpochs, useLanes);
-    h2 = figure();
+    h2 = figure('visible','off');
     plotThumbnails(expList, plotTitle, useLanes, useEpochs)
 
 %    saveMultiPage([h1,h2],['a',fileName],[false, true]);
 %    close([h1,h2]);
 
-    h3 = figure();
+    h3 = figure('visible','off');
     laserMasterPlot2(plotTitle, expList, useEpochs, useLanes);
 %	h4 = figure();
 %	sproutPlot(plotTitle, expList, useEpochs, useLanes, [1,3,5,7]);
@@ -19,7 +19,7 @@ function laserMasterPlots(plotTitle, expList, useEpochs,useLanes, fileName)
 	saveMultiPage([h1,h2,h3],['a',fileName],[false, true, false]);
 	close([h1,h2,h3]);
 
-	h4 = figure();
+	h4 = figure('visible','off');
 	sproutPlot(plotTitle, expList, useEpochs, useLanes, [1,3,5,7]);
 	saveTallPDF(['b',fileName]);
 	close(h4);
