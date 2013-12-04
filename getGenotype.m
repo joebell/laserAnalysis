6de24ab4e69a6358ffacaa03ed573ef0e2ba5fc1
joebell <joe.bell@gmail.com>
2013-12-04 09:11:32 -0500

    Added additional keys to getGenotype.m

--- getGenotype.m
+++ getGenotype.m
@@ -2,7 +2,7 @@ function val = getGenotype(key)
 
 	genoList = {'Or7a','Or10a','Gr21a','Or22a','Ir40a','Or42a',...
 			   'Or42b','Or56a','Or59b','Gr63a','Ir64a','Or67b',...
-			   'Or67d','Or82a','Or85a','Or85b','Or92a'};
+			   'Or67d','Or82a','Or83b','Or85a','Or85b','Or92a','Ctrl'};
 
 	genoMap = containers.Map();
 	for n=1:length(genoList)
